5fd26abb599c4bc526f8be0b8331da0095f0f456
Amr Sayed <amrsayed18323@gmail.com>
2026-05-24 11:33:15 +0300

    fix: enable trust proxy for correct IP detection behind Render load balancer

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 const prometheus = require('./lib/prometheus');
 
 const app = express();
+app.set('trust proxy', 1);
 const PORT = process.env.PORT || 3000;
 
 // Middlewares
