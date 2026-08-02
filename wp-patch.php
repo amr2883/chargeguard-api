597d7fa310e01b85fafa5e55b4c5e388834018dc
Amr Sayed <amrsayed18323@gmail.com>
2026-08-02 17:28:03 +0300

    Add temporary files to .gitignore and remove from tracking

--- wp-patch.php
+++
@@ -1,8 +0,0 @@
-if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-    $_SERVER['HTTP_HOST'] = $_SERVER['HTTP_X_FORWARDED_HOST'];
-}
-if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
-    $_SERVER['HTTPS'] = 'on';
-}
-define('WP_HOME', 'https://' . $_SERVER['HTTP_HOST']);
-define('WP_SITEURL', 'https://' . $_SERVER['HTTP_HOST']);
