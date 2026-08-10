169575ed5ffa93405dfa7cd4ea9737c14c58e9cc
Amr Sayed <amrsayed18323@gmail.com>
2026-08-10 13:39:17 +0300

    Cleanup: ignore temporary diagnostic files

--- check_blocks.sql
+++
@@ -1,6 +0,0 @@
-﻿SELECT p.ID, p.post_status, pm.meta_value AS block_reason
-FROM wp_posts p
-JOIN wp_postmeta pm ON p.ID = pm.post_id
-WHERE pm.meta_key = '_chargeguard_block_reason'
-ORDER BY p.ID DESC
-LIMIT 20;
