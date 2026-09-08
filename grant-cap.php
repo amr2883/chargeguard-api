6989a97a24ff743ec8a5ff95063d2d7c90aa1ac8
Amr Sayed <amrsayed18323@gmail.com>
2026-09-08 17:02:44 +0300

    chore: remove stray debug/admin scripts from repo root, not part of shipped plugin

--- grant-cap.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-require '/var/www/html/wp-load.php';
-$user = get_user_by('login', 'admin');
-if ($user) {
-    $user->add_cap('manage_woocommerce');
-    echo 'Done';
-} else {
-    echo 'User not found';
-}
--- reload-plugin.php
+++
@@ -1,6 +0,0 @@
-﻿<?php
-require '/var/www/html/wp-load.php';
-require_once ABSPATH . 'wp-admin/includes/plugin.php';
-deactivate_plugins('chargeguard/chargeguard-woocommerce.php');
-activate_plugins('chargeguard/chargeguard-woocommerce.php');
-echo 'Re-activated';
